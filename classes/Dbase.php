5517f5f49838d4a3805a9f9976d0701cae33bca6
Tapas Datta <tapash.me@gmail.com>
2016-06-18 19:21:30 +0600

    fixed the db issue

--- classes/Dbase.php
+++ classes/Dbase.php
@@ -2,47 +2,47 @@
 class Dbase {
 
 	private $_host = "localhost";
-	private $_user = "root";
-	private $_password = "password";
-	private $_name = "ecommerce";
-	
+	private $_user = "homestead";
+	private $_password = "secret";
+	private $_name = "ecom";
+
 	private $_conndb = false;
 	public $_last_query = null;
 	public $_affected_rows = 0;
-	
+
 	public $_insert_keys = array();
 	public $_insert_values = array();
 	public $_update_sets = array();
-	
+
 	public $_id;
-	
-	
+
+
 	public function __construct() {
 		$this->connect();
 	}
-	
+
 	private function connect() {
-		$this->_conndb = mysql_connect($this->_host, $this->_user, $this->_password);
-		
+		$this->_conndb = mysqli_connect($this->_host, $this->_user, $this->_password);
+
 		if (!$this->_conndb) {
 			die("Database connection failed:<br />" . mysql_error());
 		} else {
-			$_select = mysql_select_db($this->_name, $this->_conndb);
+			$_select = mysqli_select_db($this->_conndb, $this->_name);
 			if (!$_select) {
-				die("Database selection failed:<br />" . mysql_error());
+				die("Database selection failed:<br />" . mysqli_error());
 			}
 		}
-		mysql_set_charset("utf8", $this->_conndb);
+		mysqli_set_charset($this->_conndb, "utf8");
 	}
-	
-	
+
+
 	public function close() {
 		if (!mysql_close($this->_conndb)) {
 			die("Closing connection failed.");
 		}
 	}
-	
-	
+
+
 	public function escape($value) {
 		if(function_exists("mysql_real_escape_string")) {
 			if (get_magic_quotes_gpc()) {
@@ -56,54 +56,54 @@ class Dbase {
 		}
 		return $value;
 	}
-	
-	
+
+
 	public function query($sql) {
 		$this->_last_query = $sql;
-		$result = mysql_query($sql, $this->_conndb);
+		$result = mysqli_query($this->_conndb, $sql);
 		$this->displayQuery($result);
 		return $result;
 	}
-	
-	
+
+
 	public function displayQuery($result) {
 		if(!$result) {
-			$output  = "Database query failed: ". mysql_error() . "<br />";
+			$output  = "Database query failed: ". mysqli_error($this->_conndb) . "<br />";
 			$output .= "Last SQL query was: ".$this->_last_query;
 			die($output);
 		} else {
-			$this->_affected_rows = mysql_affected_rows($this->_conndb);
+			$this->_affected_rows = mysqli_affected_rows($this->_conndb);
 		}
 	}
-	
-	
+
+
 	public function fetchAll($sql) {
 		$result = $this->query($sql);
 		$out = array();
-		while($row = mysql_fetch_assoc($result)) {
+		while($row = mysqli_fetch_assoc($result)) {
 			$out[] = $row;
 		}
-		mysql_free_result($result);
+		mysqli_free_result($result);
 		return $out;
 	}
-	
+
 	public function fetchOne($sql) {
 		$out = $this->fetchAll($sql);
 		return array_shift($out);
 	}
-	
+
 	public function lastId() {
 		return mysql_insert_id($this->_conndb);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
 	public function prepareInsert($array = null) {
 		if (!empty($array)) {
 			foreach($array as $key => $value) {
@@ -112,43 +112,43 @@ class Dbase {
 			}
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
 	public function insert($table = null) {
-		
+
 		if (
-			!empty($table) && 
-			!empty($this->_insert_keys) && 
+			!empty($table) &&
+			!empty($this->_insert_keys) &&
 			!empty($this->_insert_values)
 		) {
-		
+
 			$sql  = "INSERT INTO `{$table}` (`";
 			$sql .= implode("`, `", $this->_insert_keys);
 			$sql .= "`) VALUES ('";
 			$sql .= implode("', '", $this->_insert_values);
 			$sql .= "')";
-			
+
 			if ($this->query($sql)) {
 				$this->_id = $this->lastId();
 				return true;
 			}
 			return false;
-		
+
 		}
-		
+
 	}
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	public function prepareUpdate($array = null) {
 		if (!empty($array)) {
 			foreach($array as $key => $value) {
@@ -156,14 +156,14 @@ class Dbase {
 			}
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
 	public function update($table = null, $id = null) {
 		if (!empty($table) && !empty($id) && !empty($this->_update_sets)) {
 			$sql  = "UPDATE `{$table}` SET ";
@@ -172,18 +172,18 @@ class Dbase {
 			return $this->query($sql);
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
