f8bbf4b739e28f3180d4824d48c3a002c4e8740a
Tapas Datta <tapash.me@gmail.com>
2016-06-18 19:52:01 +0600

    added phpmailer

--- classes/Email.php
+++ classes/Email.php
@@ -1,20 +1,20 @@
 <?php
-require_once('PHPMailer_v5.1/PHPMailer.php');
+require_once "vendor/autoload.php";
 
 class Email {
 
-	
+
 	private $objMailer;
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
 	public function __construct() {
-		
+
 		$this->objMailer = new PHPMailer();
 		$this->objMailer->IsSMTP();
 		$this->objMailer->SMTPAuth = true;
@@ -25,25 +25,25 @@ class Email {
 		$this->objMailer->Password = "password";
 		$this->objMailer->SetFrom("test@mail.com", "Name");
 		$this->objMailer->AddReplyTo("test@mail.com", "Name");
-		
+
 	}
 
 
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
 	public function process($case = null, $array = null) {
-	
+
 		if (!empty($case)) {
-		
+
 			switch($case) {
-				
+
 				case 1:
-				
+
 				// add url to the array
 				$link  = "<a href=\"".SITE_URL."/?page=activate&code=";
 				$link .= $array['hash'];
@@ -52,77 +52,77 @@ class Email {
 				$link .= $array['hash'];
 				$link .= "</a>";
 				$array['link'] = $link;
-				
+
 				$this->objMailer->Subject = "Activate your account";
-				
+
 				$this->objMailer->MsgHTML($this->fetchEmail($case, $array));
 				$this->objMailer->AddAddress(
-					$array['email'], 
+					$array['email'],
 					$array['first_name'].' '.$array['last_name']
 				);
-				
+
 				break;
-				
+
 			}
-			
-			
+
+
 			// send email
 			if ($this->objMailer->Send()) {
 				$this->objMailer->ClearAddresses();
 				return true;
 			}
 			return false;
-			
-		
+
+
 		}
-	
-	
+
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
 	public function fetchEmail($case = null, $array = null) {
-	
+
 		if (!empty($case)) {
-			
-			if (!empty($array)) {			
+
+			if (!empty($array)) {
 				foreach($array as $key => $value) {
 					${$key} = $value;
-				}			
+				}
 			}
-			
+
 			ob_start();
 			require_once(EMAILS_PATH.DS.$case.".php");
 			$out = ob_get_clean();
 			return $this->wrapEmail($out);
-		
+
 		}
-	
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
 	public function wrapEmail($content = null) {
 		if (!empty($content)) {
 			return "<div style=\"font-family:Arial,Verdana,Sans-serif;font-size:12px;color:#333;line-height:21px;\">{$content}</div>";
 		}
 	}
-	
-	
+
+
 
 
 
